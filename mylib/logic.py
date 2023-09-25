137710bc586092f6b043e3396fc595edabda53f2
Mustafa Cihad Göktepe <106470690+mcgmed@users.noreply.github.com>
2023-09-25 14:41:29 +0000

    latest

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,9 +1,9 @@
 import wikipedia
+from textblob import TextBlob
 
 
 def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
-
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
 
@@ -12,3 +12,9 @@ def search_wiki(name):
     """Search Wikipedia for names."""
     results = wikipedia.search(name)
     return results
+
+def phrase(name):
+    """Returns phrases from wikipedia"""
+    page = wiki(name)
+    blob = TextBlob(page)
+    return blob.noun_phrases
